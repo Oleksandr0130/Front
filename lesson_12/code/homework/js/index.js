c5b2d4e14f3731dd1304debb4e518f714e043b9b
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-04-18 16:51:18 +0200

    new

--- lesson_12/code/homework/js/index.js
+++ lesson_12/code/homework/js/index.js
@@ -1,14 +1,17 @@
-document.getElementById('bmiForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var height = parseFloat(document.getElementById('height').value);
-    var weight = parseFloat(document.getElementById('weight').value);
+document.querySelector('#bmiForm').addEventListener('submit', formSubmitHandler);
 
-    var bmi = calculateBMI(weight, height);
-    var status = getStatus(bmi);
+function formSubmitHandler(e){
+    console.log(e);
+    e.preventDefault();
+    const height = +document.getElementById('height').value;
+    const weight = +document.getElementById('weight').value;
+
+    const bmi = calculateBMI(weight, height);
+    const status = getStatus(bmi);
 
     document.getElementById('bmiResult').innerText = "Твой IMT: " + bmi.toFixed(2);
     document.getElementById('bmiStatus').innerText = "Статус: " + status;
-});
+}
 
 function calculateBMI(weight, height) {
     return weight / Math.pow(height / 100, 2);
