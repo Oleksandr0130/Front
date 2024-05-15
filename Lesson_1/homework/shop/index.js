fc61527095a5372f3423da1df6d76e93f1bb7340
вашÐOleksandr0130 <olek1995sta@gmail.com>
2024-05-15 21:26:00 +0200

    new

--- Lesson_1/homework/shop/index.js
+++ Lesson_1/homework/shop/index.js
@@ -1,10 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-    addToCartButtons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            alert("Товар добавлен в корзину!"); // Пример реакции на клик
-            // Здесь можно добавить логику для добавления товара в корзину
-        });
-    });
+const products = [
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 1000,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-gaming-fury.jpg',
+        price: 2000,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 3005,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 3060,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 300,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 300,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 300,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+    {
+        image: '../images/hyperpc-one-deepcool.jpg',
+        price: 300,
+        description: 'Компьютер: модель One <br> Процессор: Intel Core i5 <br> 16 ГБ оперативной памяти <br> 512 ГБ SSD',
+    },
+
+];
+
+const productList = document.getElementById('product-list');
+
+products.forEach(product => {
+    const listItem = document.createElement('li');
+
+    const img = document.createElement('img');
+    img.src = product.image;
+    img.className = 'foto';
+    listItem.appendChild(img);
+
+    const productInfo = document.createElement('div');
+    productInfo.className = 'product-info';
+
+    const description = document.createElement('p');
+    description.innerHTML = product.description;
+    productInfo.appendChild(description);
+
+    const price = document.createElement('p');
+    price.textContent = `Цена: ${product.price} руб.`;
+    productInfo.appendChild(price);
+
+    const button = document.createElement('button');
+    button.textContent = 'Добавить в корзину';
+    button.onclick = () => addToCart(product);
+    productInfo.appendChild(button);
+
+    listItem.appendChild(productInfo);
+
+    productList.appendChild(listItem);
 });
+function addToCart(product) {
+    console.log('Товар добавлен в корзину:', product);
+    alert(`Товар добавлен в корзину: ${product.description.replace(/<br>/g, '\n')}`);
+}
